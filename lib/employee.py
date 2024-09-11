b5137c0722b8605131a0c2c2686353f296ebb856
Abiud Kiprotich <abiud.kiprotich@student.moringaschool.com>
2024-09-11 21:54:42 +0300

    completed the lab

--- lib/employee.py
+++ lib/employee.py
@@ -1,6 +1,7 @@
 # lib/employee.py
 from __init__ import CURSOR, CONN
 from department import Department
+import sqlite3
 
 class Employee:
 
@@ -187,4 +188,14 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review import Review
+        sql = """
+            SELECT * FROM reviews
+            WHERE employee_id = ?
+        """
+        CURSOR.execute(sql, (self.id,),)
+
+        rows = CURSOR.fetchall()
+        return [
+            Review.instance_from_db(row) for row in rows
+        ]
